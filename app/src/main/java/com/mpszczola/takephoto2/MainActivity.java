b87e0fe1b19e9bede246ed850c1a41fa2e75b58a
Maria Pszczola <mariapszczola@gmail.com>
2019-05-27 23:33:46 +0200

    OCR dziala na starszych emulatorach

--- app/src/main/java/com/mpszczola/takephoto2/MainActivity.java
+++ app/src/main/java/com/mpszczola/takephoto2/MainActivity.java
@@ -4,25 +4,31 @@ package com.mpszczola.takephoto2;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.os.Environment;
 import android.widget.TextView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private Button takePictureButton;
     private ImageView imageView;
     private Uri fileUri;
+    String datapath = "";
     // dodane potem:
     Bitmap image;
     private TessBaseAPI mTess;
@@ -49,11 +56,12 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageview);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            takePictureButton.setEnabled(false);
+            //takePictureButton.setEnabled(false);
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
 
-
+        datapath = getFilesDir()+ "/tesseract/";
+        checkFile(new File(datapath + "tessdata/"));
     }
 
     @Override
@@ -83,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
         return new File(sciezka);
     }
 
+
     public void takePicture(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         fileUri = Uri.fromFile(getOutputMediaFile());
@@ -102,7 +111,6 @@ public class MainActivity extends AppCompatActivity {
                 //imageView.setImageURI(fileUri);
                 image = BitmapFactory.decodeFile(fileUri.getPath());
                 mTess = new TessBaseAPI();
-                String datapath = getFilesDir()+ "/tesseract/";
                 mTess.init(datapath, "eng");
                 mTess.setImage(image);
                 String text = mTess.getUTF8Text();
@@ -114,7 +122,44 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
 
+    private void checkFile(File dir) {
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles();
+        }
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+
+    private void copyFiles() {
+        try {
+            String filepath = datapath + "/tessdata/eng.traineddata";
+            AssetManager assetManager = getAssets();
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+            File file = new File(filepath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
